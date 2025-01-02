5f9b9e0a647b503c8942add81518388327fb8f82
Min-Seok-Kim <rladbsdnjs87@naver.com>
2025-01-02 21:53:55 +0900

    first commit

---
+++ src/main/java/com/example/YoonONI_BackEnd/YoonOniBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.YoonONI_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonOniBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonOniBackEndApplication.class, args);
+	}
+
+}
