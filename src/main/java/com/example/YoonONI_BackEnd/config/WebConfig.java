089ba9a2d74b77d0a31c7b49fc56771bb3576638
Min-Seok-Kim <rladbsdnjs87@naver.com>
2025-07-26 18:23:51 +0900

    cors 설정
    - 요청 오류로 인한 cors 설정 추가

--- src/main/java/com/example/YoonONI_BackEnd/config/WebConfig.java
+++ src/main/java/com/example/YoonONI_BackEnd/config/WebConfig.java
@@ -4,6 +4,7 @@ package com.example.YoonONI_BackEnd.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(requestArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 프론트 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
--- src/main/java/com/example/YoonONI_BackEnd/config/security/SecurityConfig.java
+++ src/main/java/com/example/YoonONI_BackEnd/config/security/SecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -46,6 +51,20 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true); // 쿠키나 인증 정보 등을 함께 보낼 수 있도록 허용
+        configuration.setAllowedHeaders(List.of("*")); // 프론트에서 보낼 수 있는 헤더 목록을 지정
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -68,6 +87,7 @@ public class SecurityConfig {
                         auth.requestMatchers("/signup", "/login").permitAll()
                                 .anyRequest().authenticated()
                 );
+        http.cors((cors) -> cors.configurationSource(corsConfigurationSource()));
 
         http.authenticationProvider(authenticationProvider());
 
